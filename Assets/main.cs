33ea6ac586c546a729136122b8847e4da2af14e1
Callum Parker <callum.parker@gmail.com>
2018-04-17 22:48:45 +1000

    New commit
    Adding code.

---
+++ Assets/main.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class main : MonoBehaviour {
+
+    public GameObject menuItems;
+    public GameObject observationButtons;
+    public InputField iField;
+    public Text observationLabel;
+
+    public bool menuEnabled = true;
+    private string observationName = "";
+
+    FileInfo f;
+    private string observationData = "";
+    private string startTime = "";
+    private string startTimeNormal = "";
+
+    private System.DateTime epochStart = new System.DateTime(1970, 1, 1, 0, 0, 0, System.DateTimeKind.Utc);
+
+
+    private int passerby = 0,
+    noticed = 0,
+    smartphone = 0,
+    smartphoneNoticed = 0,
+    interacted = 0,
+    smartphoneInteracted = 0;
+
+
+    // Update is called once per frame
+    void Update () {
+        // Turn of main menu
+        if (menuEnabled == false)
+        {
+            menuItems.SetActive(false);
+            observationButtons.SetActive(true);
+        }
+	}
+
+    public void observationCount(string countType)
+    {
+        switch (countType)
+        {
+            case "passer-by":
+                passerby++;
+                break;
+            case "noticed":
+                noticed++;
+                break;
+            case "smartphone":
+                smartphone++;
+                break;
+            case "smartphonenoticed":
+                smartphoneNoticed++;
+                break;
+            case "interacted":
+                interacted++;
+                break;
+            case "smartphoneinteracted":
+                smartphoneInteracted++;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void startObservation()
+    {
+        int cur_time = (int)(System.DateTime.UtcNow - epochStart).TotalSeconds;
+        startTime = cur_time.ToString();
+        startTimeNormal = System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
+        observationName = iField.text;
+        observationLabel.text = observationName;
+        Debug.Log("Observation name set to: " + observationName);
+        menuEnabled = false;
+    }
+
+
+    public void endObservation()
+    {
+        int cur_time = (int)(System.DateTime.UtcNow - epochStart).TotalSeconds;
+        f = new FileInfo(Application.persistentDataPath + "\\" + observationName + "_" + cur_time.ToString() + ".txt");
+        Save();
+    }
+
+    void Save()
+    {
+        StreamWriter w;
+        if (!f.Exists)
+        {
+            w = f.CreateText();
+        }
+        else
+        {
+            f.Delete();
+            w = f.CreateText();
+        }
+
+        observationData = "\nPassers-by: " + passerby.ToString() + "\nNoticed: " + noticed.ToString() + "\nInteracted: " + interacted.ToString() + "\nSmartphone Users: " + smartphone.ToString() + "\nSmartphone & Noticed: " + smartphoneNoticed.ToString() + "\nSmartphone & Interacted: " + smartphoneInteracted.ToString() + "\n";
+
+        w.WriteLine(observationName + "\n" + startTimeNormal + "\n\n" + observationData + "\n\n" + System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"));
+        w.Close();
+
+        Application.Quit();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# observation-behaviour-counter
-A counting app in Unity for counting behaviours during field observations on a smartphone.
+# Field Observation Behaviour Counter
+A behaviour counting app created with Unity. It is used for observation studies to make counting people and their behaviours easier.
+
+Requirements:
+- Unity 5.6.2 or higher.
+- Unity build tools for Android or iOS.
+
+This app was created specifically to run on an Android device. However, it will probably work just fine on an iOS device thanks to the power of Unity's build tools! :-)
